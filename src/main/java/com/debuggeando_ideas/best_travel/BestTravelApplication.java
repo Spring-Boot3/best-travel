f64b32c4b353b4e5fbd8ff7dec5ae55fda73f677
HectorARG <hectorr9577@gmail.com>
2024-04-29 19:55:07 -0600

    Se incorporan Repository para pruebas de conexion

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -1,13 +1,29 @@
 package com.debuggeando_ideas.best_travel;
 
+import com.debuggeando_ideas.best_travel.domain.repositories.FlyRepository;
+import com.debuggeando_ideas.best_travel.domain.repositories.HotelRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BestTravelApplication {
+@Slf4j
+public class BestTravelApplication implements CommandLineRunner {
+
+	@Autowired
+	private FlyRepository flyRepository;
+
+	@Autowired
+	private HotelRepository hotelRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestTravelApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.debug("Holaaaa");
+	}
 }
