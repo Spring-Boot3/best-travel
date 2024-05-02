74426c88e964403f5bc356067d3412a29bb478ac
Héctor Adrián Román García <hector.romang@durango.gob.mx>
2024-05-01 18:07:29 -0600

    Se agregan cambios en archivo principal Bugs

--- src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -1,6 +1,6 @@
 package com.debuggeando_ideas.best_travel;
 
-
+import com.debuggeando_ideas.best_travel.domain.repositories.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,12 +10,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class BestTravelApplication implements CommandLineRunner {
 
+	private final FlyRepository flyRepository;
+
+	private final HotelRepository hotelRepository;
+
+	private final CustomerRepository customerRepository;
+
+	private final ReservationRepository reservationRepository;
+
+	private final TicketRepository ticketRepository;
+
+	private final TourRepository tourRepository;
+
+	BestTravelApplication(
+			FlyRepository flyRepository,
+			HotelRepository hotelRepository,
+			CustomerRepository customerRepository,
+			ReservationRepository reservationRepository,
+			TicketRepository ticketRepository,
+			TourRepository tourRepository
+	) {
+		this.flyRepository = flyRepository;
+		this.hotelRepository = hotelRepository;
+		this.customerRepository = customerRepository;
+		this.reservationRepository = reservationRepository;
+		this.ticketRepository = ticketRepository;
+		this.tourRepository = tourRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BestTravelApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		log.debug("Holaaaa");
+		var fly = flyRepository.findById(15L).get();
+		var hotel = hotelRepository.findById(7L).get();
 	}
 }
