fdc8bc56bed7067ff8bdf202c5f38eeb33a1eac6
HectorARG <hectorr9577@gmail.com>
2024-05-05 21:13:56 -0600

    Mapeando relaciones inversas

--- src/main/java/com/debuggeando_ideas/best_travel/domain/entities/TourEntity.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/entities/TourEntity.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.UUID;
 
 @Entity(name = "tour")
 @NoArgsConstructor
@@ -38,7 +41,33 @@ public class TourEntity implements Serializable {
     @JoinColumn(name = "id_customer")
     private CustomerEntity customer;
 
-    public void addTicket(){
-        
+    public void addTicket(TicketEntity ticket){
+        if(Objects.isNull(this.tickets)) this.tickets = new HashSet<>();
+        this.tickets.add(ticket);
+    }
+
+    public void removeTicket(UUID id){
+        //Aquí es una funcion lambda que removera el ticket solo si se cumple la condicion
+        if(Objects.isNull(this.tickets)) this.tickets = new HashSet<>();
+        this.tickets.removeIf(ticket -> ticket.getId().equals(id));
+    }
+
+    public void updateTickets(){
+        this.tickets.forEach(ticket -> ticket.setTour(this));
+    }
+
+    public void addReservation(ReservationEntity reservation){
+        if(Objects.isNull(this.reservations)) this.reservations = new HashSet<>();
+        this.reservations.add(reservation);
+    }
+
+    public void removeReservation(UUID id){
+        //Aquí es una funcion lambda que removera el ticket solo si se cumple la condicion
+        if(Objects.isNull(this.reservations)) this.reservations = new HashSet<>();
+        this.reservations.removeIf(reservation -> reservation.getId().equals(id));
+    }
+
+    public void updateReservation(){
+        this.reservations.forEach(reservation -> reservation.setTour(this));
     }
 }
