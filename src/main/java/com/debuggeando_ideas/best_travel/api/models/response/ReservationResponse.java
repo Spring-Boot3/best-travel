8b20afb124f98965e5ea485c3125cb80ec1ca1e5
HectorARG <hectorr9577@gmail.com>
2024-05-16 20:06:52 -0600

    Se agrega DTO de Reservation

--- src/main/java/com/debuggeando_ideas/best_travel/api/models/response/ReservationResponse.java
+++ src/main/java/com/debuggeando_ideas/best_travel/api/models/response/ReservationResponse.java
@@ -7,17 +7,21 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public class HotelResponse implements Serializable {
+public class ReservationResponse implements Serializable {
 
-    private Long id;
-    private String name;
-    private String address;
-    private Integer rating;
+    private UUID id;
+    private LocalDateTime dateTimeReservation;
+    private LocalDate dateStart;
+    private LocalDate dateEnd;
+    private Integer totalDays;
     private BigDecimal price;
 
 }
