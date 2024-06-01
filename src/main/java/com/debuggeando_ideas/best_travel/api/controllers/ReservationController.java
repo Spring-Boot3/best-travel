886422c9d5ec10b86820dcdff971e38d6688c4e0
HectorARG <hectorr9577@gmail.com>
2024-05-31 20:50:30 -0600

    Se agregan los endpoints para el CRUD de reservation

--- src/main/java/com/debuggeando_ideas/best_travel/api/controllers/ReservationController.java
+++ src/main/java/com/debuggeando_ideas/best_travel/api/controllers/ReservationController.java
@@ -1,7 +1,9 @@
 package com.debuggeando_ideas.best_travel.api.controllers;
 
 import com.debuggeando_ideas.best_travel.api.models.request.ReservationRequest;
+import com.debuggeando_ideas.best_travel.api.models.request.TicketRequest;
 import com.debuggeando_ideas.best_travel.api.models.response.ReservationResponse;
+import com.debuggeando_ideas.best_travel.api.models.response.TicketResponse;
 import com.debuggeando_ideas.best_travel.domain.repositories.CustomerRepository;
 import com.debuggeando_ideas.best_travel.domain.repositories.HotelRepository;
 import com.debuggeando_ideas.best_travel.domain.repositories.ReservationRepository;
@@ -9,10 +11,12 @@ import com.debuggeando_ideas.best_travel.infraestructure.abstract_services.IRese
 import com.debuggeando_ideas.best_travel.infraestructure.services.ReservationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "reservation")
@@ -24,6 +28,27 @@ public class ReservationController {
     @PostMapping
     public ResponseEntity<ReservationResponse> post(@RequestBody ReservationRequest request){
         return ResponseEntity.ok(iReservationService.create(request));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ReservationResponse> get(@PathVariable UUID id){
+        return ResponseEntity.ok(iReservationService.read(id));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ReservationResponse> put(@RequestBody ReservationRequest request, @PathVariable UUID id){
+        return ResponseEntity.ok(iReservationService.update(request, id));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        this.iReservationService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, BigDecimal>> getReservationPrice(@RequestParam Long reservationId){
+        return ResponseEntity.ok(Collections.singletonMap("price", iReservationService.findPrice(reservationId)));
     }
 
 
--- src/main/java/com/debuggeando_ideas/best_travel/domain/entities/ReservationEntity.java
+++ src/main/java/com/debuggeando_ideas/best_travel/domain/entities/ReservationEntity.java
@@ -27,13 +27,13 @@ public class ReservationEntity implements Serializable {
     private LocalDate dateEnd;
     private Integer totalDays;
     private BigDecimal price;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hotel_id")
     private HotelEntity hotel;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tour_id", nullable = true)
     private TourEntity tour;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id")
     private CustomerEntity customer;
 }
