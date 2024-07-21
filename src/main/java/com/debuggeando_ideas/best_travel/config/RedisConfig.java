cf24ea814438502e8ba7c67edadf4c273aa55f58
HectorARG <hectorr9577@gmail.com>
2024-07-21 17:45:06 -0600

    Se agrega la candelarizacion de las tareas de limpieza de cache

--- src/main/java/com/debuggeando_ideas/best_travel/config/RedisConfig.java
+++ src/main/java/com/debuggeando_ideas/best_travel/config/RedisConfig.java
@@ -10,14 +10,25 @@ import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Map;
+
+// Esta anotacion habilita la configuracion de spring
 @Configuration
+// Esta anotacion habilita el uso de cache en spring
 @EnableCaching
+// Esta anotacion habilita la programacion de tareas en spring
+@EnableScheduling
+// Esta amotacion habilita la configuracion de spring
 @Slf4j
+// Esta clase de configuracion se encarga de configurar el cliente de redis y el cache manager
 public class RedisConfig {
 
     @Value(value = "cache.redis.address")
@@ -48,6 +59,24 @@ public class RedisConfig {
                 CacheConstants.HOTEL_CACHE_NAME, new CacheConfig()
         );
         return new RedissonSpringCacheManager(redissonClient, configs);
+    }
+
+    /*
+     * Con esta configuracion podemos programar tareas en spring
+     */
+    // Esta anotacion es para limpiar el cache de redis
+    @CacheEvict(
+            cacheNames = {CacheConstants.FLY_CACHE_NAME, CacheConstants.HOTEL_CACHE_NAME},
+            allEntries = true
+    )
+    @Scheduled(cron = CacheConstants.SCHEDULED_RESET_CACHE)
+    // Esta anotacion habilita la ejecucion de metodos
+    // de forma asincrona lo que permite que el metodo
+    // se ejecute en un hilo diferente
+    @Async
+    public void deleteCache() {
+        log.info("Cache deleted");
+
     }
 
 }
